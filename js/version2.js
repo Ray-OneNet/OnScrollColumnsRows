a45ecdaef7443a6cb4eb6b0c2c03c3a6f95f547a
Ray-OneNet <132604900+Ray-OneNet@users.noreply.github.com>
2024-01-04 14:09:04 -0800

    demo-2

---
+++ js/version2.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+    // Your code here
+    gsap.registerPlugin(CustomEase) 
+    gsap.defaults({
+        ease: "none",
+    
+    
+    });
+    gsap.set(".loader_column-inner",{
+        width: "100vw",
+        height: "350%",
+    })
+    gsap.set(".loader_flex",{
+        scale: 0.25,
+    })
+    gsap.set(".loader_column-inner.is-reverse",{
+        y: "-40%",
+    })
+    gsap.set(".loader_column-inner.is-cented",{
+        y: "40%",
+    })
+    gsap.set(".loader_column-inner.is-edge",{
+        y: "70%",
+    })
+    gsap.set(".loader_img-wrap",{
+        scale: 0.95,
+    })
+
+    let tl=gsap.timeline()
+    .to("main",{
+        opacity: 1,
+        duration: 1
+    })
+    .to(".loader_column-inner",{
+        width: "100vw",
+        height: "100%",
+        duration: 2.5,
+        ease: "sine.inOut",
+    })
+    .to(".loader_column-inner.is-cented",{
+        y: "0%",
+        duration: 2.5,
+        ease: "power1.inOut",
+    },"<")
+    .to(".loader_column-inner.is-reverse",{
+        y: "10%",
+        duration: 2.5,
+        ease: "power1.inOut",
+    },"<+0.25")
+    .to(".loader_column-inner.is-edge",{
+        y: "0%",
+        duration: 2.5,
+        ease: "power1.inOut",
+    },"<+0.25")
+    .to(".loader_img-wrap > img, .loader_img-wrap > video",{
+        scale: 1,
+        duration: 2,
+        ease: "power3.inOut",
+    },">-1")
+    .to(".loader_img-wrap#main-img",{
+        scale: 1,
+        duration: 2,
+        ease: "power3.inOut",
+    },"<")
+    .to(".loader_flex",{
+        scale: 1,
+        duration: 2,
+        ease: "power3.inOut",
+    },"<")
+    
+};
